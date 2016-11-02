f9c82a4a27a4addafdb609f2db6a67807ad37be5
Marcelo Boeira <me@marceloboeira.com>
2016-11-02 15:53:15 +0100

    test: Use the tempdir package to create the repositories

--- spec/git/repository_spec.cr
+++ spec/git/repository_spec.cr
@@ -1,40 +1,39 @@
 require "../spec_helper"
+require "tmpdir"
 
 describe Git::Repository do
   describe "init" do
     describe "when no path is given" do
       it "initializes in the current folder" do
-        repository_path = "/tmp/foo/bar/#{SecureRandom.hex}"
-        Dir.mkdir_p(repository_path)
-        Dir.cd(repository_path)
+        Dir.mktmpdir do |repository_path|
+          Dir.cd(repository_path)
 
-        respository = Git::Repository.init
+          respository = Git::Repository.init
 
-        Dir.exists?("#{repository_path}/.git").should eq(true)
-
-        FileUtils.rm_r(repository_path)
+          Dir.exists?("#{repository_path}/.git").should eq(true)
+        end
       end
     end
 
     describe "when the path is given" do
       describe "and it is valid" do
         it "initializes in the given folder" do
-          repository_path = "/tmp/foo/bar/#{SecureRandom.hex}"
-          Dir.mkdir_p(repository_path)
-          Dir.cd("/tmp")
+          Dir.mktmpdir do |repository_path|
+            Dir.cd("/var")
 
-          Git::Repository.init(repository_path)
+            Git::Repository.init(repository_path)
 
-          Dir.exists?("#{repository_path}/.git").should eq(true)
-
-          FileUtils.rm_r(repository_path)
+            Dir.exists?("#{repository_path}/.git").should eq(true)
+          end
         end
       end
 
       describe "and it is invalid" do
         it "raises an expection" do
           expect_raises(Git::Repository::InvalidPathException) do
-            Git::Repository.init("/foo/bar/b")
+            Dir.mktmpdir do |repository_path|
+              Git::Repository.init("/foo/bar/b")
+            end
           end
         end
       end
@@ -45,16 +44,14 @@ describe Git::Repository do
     describe "and the given URI is valid" do
       describe "and it is https" do
         it "creates a new folder with the repository" do
-          uri = "https://github.com/marceloboeira/git.cr.git"
-          repository_dir = "/tmp/foo/bar/#{SecureRandom.hex}"
-          Dir.mkdir_p(repository_dir)
-          Dir.cd(repository_dir)
+          Dir.mktmpdir do |repository_path|
+            uri = "https://github.com/marceloboeira/git.cr.git"
+            Dir.cd(repository_path)
 
-          respository = Git::Repository.clone(uri)
+            respository = Git::Repository.clone(uri)
 
-          Dir.exists?("#{repository_dir}/git.cr/.git").should eq(true)
-
-          FileUtils.rm_r(repository_dir)
+            Dir.exists?("#{repository_path}/git.cr/.git").should eq(true)
+          end
         end
       end
     end
@@ -63,7 +60,9 @@ describe Git::Repository do
       it "raises an exception" do
         uri = "http://invalid.com/foo/bar.git"
         expect_raises(Git::Repository::InvalidURIException) do
-          respository = Git::Repository.clone(uri)
+          Dir.mktmpdir do |repository_path|
+            respository = Git::Repository.clone(uri)
+          end
         end
       end
     end
